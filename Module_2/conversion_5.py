df4be7d07842859b3ddd7fb7ad60c545ad0df180
ayziyz <ayusht@metropoli.fi>
2024-08-23 13:48:02 +0545

    Changed the few lines of code

--- Module_2/conversion_5.py
+++ Module_2/conversion_5.py
@@ -1,18 +1,10 @@
-# Given
-talents_to_pounds = 20
-pounds_to_lots = 32
-lots_to_grams = 13.3
 
 talents = float(input("Enter the talents: "))
 pounds = float(input("Enter the pounds: "))
 lots = float(input("Enter the lots: "))
 
-total_pounds = (talents_to_pounds * talents) + pounds
-total_lots = (pounds_to_lots * pounds) + lots
-total_grams = total_lots * lots_to_grams
+kilograms = ((talents*20+pounds)*32 + lots)*0.0133
+grams = 1000.0 *(kilograms - int(kilograms))
 
-kilograms = total_grams // 1000
-grams = total_grams % 1000
-
-print(f"{kilograms} kilograms and {grams} grams")
+print(f"The weight in modern units:\n{int (kilograms)} kilograms and {grams: .2f} grams")
 
